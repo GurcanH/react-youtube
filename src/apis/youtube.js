caa2c2532b8b669f2081ad68fc496b60fa3649ac
Gurcan Hamali <gurcanhamali@gmail.com>
2020-11-03 23:08:42 +1100

    119. Searching for Videos

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,1 +1,13 @@
+import axios from 'axios';
+
 const KEY = 'AIzaSyBLXsE23cawhzNWMpSoVwrbKwJiD2KS2vw';
+
+export default axios.create({
+  baseURL: 'https://www.googleapis.com/youtube/v3',
+  params: {
+    part: 'snippet',
+    type: 'video',
+    maxResults: 5,
+    key: KEY
+  }
+});
